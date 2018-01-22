37986f5f75f715e4224edc3e6102b0d60d103f91
Imad Merveil <imadmerveil@imads-mbp-2016.nvstu.edu>
2018-01-22 14:50:59 -0500

    first commit

---
+++ Odd.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Odd {
+	public static void main(String[] args) {
+	Scanner keyboard = new Scanner(System.in);	
+	for(int i=0; i<=100; i++) {
+		if(i%2==1) {
+			System.out.println(i);		}
+	}
+	}
+
+}
