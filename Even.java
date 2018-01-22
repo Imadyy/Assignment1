43deb8ba0c3dfc719957ec56902bb91df349dd73
Imad Merveil <imadmerveil@imads-mbp-2016.nvstu.edu>
2018-01-22 14:44:34 -0500

    first commit

---
+++ Even.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Even {
+	public static void main(String[] args) {
+	Scanner keyboard = new Scanner(System.in);
+	int a=0; 
+	do {
+		a+=2;
+		System.out.println(a);
+	} while(a<100);
+	
+   }
+}
+
+
